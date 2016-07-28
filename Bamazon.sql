db13e1feb2f04b5977d4494a59d43e1081e54119
jrizza88 <jetorres88@gmail.com>
2016-07-28 01:14:48 -0400

    initial files created

---
+++ Bamazon.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE products (
+id INT NOT NULL AUTO_INCREMENT, 
+productName VARCHAR(50) NOT NULL,
+departmentName VARCHAR(50) NOT NULL,
+price INTEGER,
+stockQuantity INTEGER,
+PRIMARY KEY (id)
+);
+
+-- DROP TABLE products;
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("jetblack flatscreen tv", "technology", 2000, 200);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("haxbook notepad", "technology", 950, 90);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("purple tonic keyboard", "technology", 200, 400);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("red haxbook laptop", "technology", 2500, 100);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("art canvas", "art supplies", 150, 150);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("paint brush", "art supplies", 10, 2000);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("colors array set", "art supplies", 75, 1500);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("blue hoodie", "clothing", 80, 700);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("white shorts", "clothing", 60, 300);
+
+INSERT INTO products (productName, departmentName, price, stockQuantity)
+VALUES ("classic snapback hat", "clothing", 35, 475);
+
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,114 @@
+// require node packages (mySQL and prompt) in order to complete the assignment
+var mysql = require('mysql');
+var prompt = require('prompt');
+// using these node packages for better looking colors and organization for the application
+var colors = require('colors');
+var cliTable = require('cli-table');
+
+// create the connection to the database that I want to use 
+var connection = mysql.createConnection({
+// must use correct objects which are -  host, user, password, port, database
+host: 'localhost',
+user: 'root',
+password: 'orange88',
+port: 3306,
+database: 'Bamazon'
+});
+
+var catalogue = 
+          ["jetblack flatscreen tv", "haxbook notepad",
+           "purple tonic keyboard", "red haxbook laptop",
+           "art canvas", "paint brush", "colors array set",
+           "blue hoodie", "white shorts"];
+
+// checking to see if the catalogue gets listed
+  console.log("These are the list of all items in the catalogue: ", catalogue);
+
+// checks for connection error
+connection.connect(function(err){
+  if (err) {
+    throw err;
+  }
+// informs user that the connection to the application and database was successful
+  console.log('Connection to Bamazon successful!');
+});
+
+// ****use queries to test out and run the different required components of the application***** \\
+
+ // using prompt here to ask the two questions to the customer. 
+
+ 
+
+  var customerQuestions = [{
+     //Question 1: ID of the product they would like to buy
+     name: "id",
+     type: "rawlist",
+     message: "which product id number would you like?",
+     choices: catalogue
+    },{
+   // Questions 2: how many units of the product they would like to buy
+    name: "numUnits",
+    message: "how many units would you like?"
+    }];
+
+
+                    function showCatalogue() {
+                          connection.query('SELECT * FROM products', function(err, results) {
+                            if (err) {
+                              throw err;
+                            } else {
+                                results.forEach (function(row) {
+                                  console.log(""  + "\nId: " + row.id + "\nProduct: " + row.productName +
+                                   "\nDepartment: " + row.departmentName + "\nPrice: " + row.price +
+                                   "\nStock: " + row.stockQuantity );
+                                  console.log("\n------------------------------------");
+                            })
+                                promptCatalogue();
+                          }
+                        })
+                    };
+
+
+
+function promptCatalogue(){
+  inquirer.prompt(customerQuestions).then(function (results){
+              var productIdInput = results.id;
+              var productInventoryInput = results.numUnits;
+              console.log(productIdInput);
+              console.log(productInventoryInput);
+            connection.query('SELECT * FROM products WHERE ?',{
+                        ID: productIdInput,
+            }, function (err, rows) {
+              if (err){
+                throw err;
+              } else {
+    // if not enough QUANTITY console.log ("Insufficient quantity!");
+                rows.forEach(function(row){
+                  if (row.stockQuantity === 0){
+                  console.log("Insufficient quantity!")
+                } else {
+                  console.log("There are still enough products")
+                }
+                 
+              })
+            }
+          })
+        });
+      }
+showCatalogue();
+// Once the customer has placed the order, your application 
+//should check if your store has enough of the product to meet the customer's request. (use for loop)
+
+  
+    // if there is enough of the product, order needs to be fulfilled here||| 
+            //1. update the SQL DB to reflect remaining quantity
+            //2. Once update goes through, show customer total cost of their purchase..
+
+
+// YOU FINISHED!
+
+
+
+
+
+
